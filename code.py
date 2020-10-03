1e330e5cecc10ed2e922a76babdca35cc97c5147
Asm Saikat <67100877+AsmSaikat@users.noreply.github.com>
2020-10-03 12:14:05 +0600

    Update code.py

--- code.py
+++ code.py
@@ -1,18 +1,20 @@
 import pyttsx3
 import PyPDF2
 
-book = open(input("Please type you book's name with extension: ") + ".pdf", "rb")
-# rb: Opens the file as read-only in binary format and starts reading from the beginning of the file
+book = open(input("Please type you book's name: ") + ".pdf", "rb")
+# rb: Opens the file as read-only in binary format and starts reading from the beginning of the file.
 
+# this will initialize the PdfFileReader object.
 pdf = PyPDF2.PdfFileReader(book)
 
-# init function to get an engine instance for the speech synthesis
+# using init function to get an engine instance for the speech synthesis.
 engine = pyttsx3.init()
 
-print("Please choose pages you want to create audiobook with:")
+print("Please choose pages you want to create audiobook with-")
 start = input("Start: ")
 end = input("End: ")
 
+# now we will try verifying whether the start or end input is valid or not.
 try:
     int(start) and int(end)
 except ValueError:
@@ -26,6 +28,21 @@ for num in range(int(start), int(end)):
 
     # using extractText() to extract texts from pages.
     text_extractor = page.extractText()
+
+    # retrieving the value and selecting the WPM(Words Per Minute) rate of voices.
+    voices = engine.getProperty("voices")
+    rate = engine.setProperty("rate", int(input("Please select WPM: ")))
+    
+    # selecting voice.
+    print("Which voice would you prefer?")
+    select = input("Type M for Male Voice or Type F for Female Voice: ")
+    if select == "M" or select == "m":
+        engine.setProperty("voice", voices[0].id)
+    elif select == "F" or select == "f":
+        engine.setProperty("voice", voices[1].id)
+    else:
+        print("Please use a valid input by running the program again.")
+        break
 
     # say method on the engine that passing input text to be spoken.
     engine.say(text_extractor)
